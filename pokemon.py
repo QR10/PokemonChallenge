2b5079e7956176eaf9f786cba693fbc962181af7
Queli Serrano <quelirenato34@gmail.com>
2020-11-03 21:59:36 +0000

    Delegated some tasks into new functions and changed some variable names to make code more readable

--- pokemon.py
+++ pokemon.py
@@ -1,33 +1,28 @@
 # takes move sequence and returns dimensions for the grid
-def numOfMovements(moveSequence):
-    # Turn string into a list of characters
-    movements = list(moveSequence.lower())[::-1]
+def get_map_size(ash_movement_sequence):
+
+    list_of_ash_movements = list(ash_movement_sequence.lower())[::-1]
     
     down = 0
     up = 0
     right = 0
     left = 0
     
-    # Enquanto ouver movimentos
-    while movements:
+    while list_of_ash_movements:
         # retirar 1 movimento e processar
-        move = movements.pop()
+        ash_movement = list_of_ash_movements.pop()
     
-        if move == "s":
-            # move down
+        if ash_movement == "s":
             down += 1
-        elif move == "n":
-            # move up
+        elif ash_movement == "n":
             up += 1
-        elif move == "e":
-            # move right
+        elif ash_movement == "e":
             right +=1
-        elif move == "o":
-            # move left
+        elif ash_movement == "o":
             left += 1
         else:
             # ignore wrong input
-            print("Invalid move")
+            print(f"{ash_movement} is not a valid move!")
     
     # Guardar maior numero de movimentos numa direcao
     if right > left:
@@ -42,68 +37,69 @@ def numOfMovements(moveSequence):
     
     return x, y
     
-def solve(moveSequence):
-    x, y = numOfMovements(moveSequence)
-    
-    # String to list of characters 
-    movements = list(moveSequence.lower())[::-1]
-    
-    # Generating 2d World according to movements
+def generate_map(x, y) :
+    # Generating 2d map according to movements
     world = []
-    for j in range(x*2):
+    for j in range(x*2+1):
         column = []
-        for i in range(y*2):
+        for i in range(y*2+1):
             column.append(1)
         world.append(column)
         
     # Ash starts here and gets starting Pokemon
     world[x][y] = 0 # center
-    pokeCount = 1
+        
+    return world
+
+def catch_pokemon(grid_of_houses, x, y):
+    if grid_of_houses[x][y] == 1:
+        grid_of_houses[x][y] = 0
+        return 1
+    else:
+        return 0
+
+def solve(ash_movement_sequence):
+    x, y = get_map_size(ash_movement_sequence)
+    
+    list_of_ash_movements = list(ash_movement_sequence.lower())[::-1]
+    
+    grid_of_houses = generate_map(x, y) 
+
+    pokemon_count = 1
     
     # Enquanto ouver movimentos
-    while movements:
+    while list_of_ash_movements:
         # retirar 1 movimento e processar
-        move = movements.pop()
+        ash_movement = list_of_ash_movements.pop()
     
-        if move == "s":
-            # move down
+        if ash_movement == "s":
             y = y+1
-            if world[x][y] == 1:
-                world[x][y] = 0
-                pokeCount += 1
-        elif move == "n":
-            # move up
+            pokemon_count += catch_pokemon(grid_of_houses, x, y)
+        elif ash_movement == "n":
             y = y-1
-            if world[x][y] == 1:
-                world[x][y] = 0
-                pokeCount += 1
-        elif move == "e":
-            # move right
+            pokemon_count += catch_pokemon(grid_of_houses, x, y)
+        elif ash_movement == "e":
             x = x+1
-            if world[x][y] == 1:
-                world[x][y] = 0
-                pokeCount += 1
-        elif move == "o":
-            # move left
+            pokemon_count += catch_pokemon(grid_of_houses, x, y)
+        elif ash_movement == "o":
             x = x-1
-            if world[x][y] == 1:
-                world[x][y] = 0
-                pokeCount += 1
+            pokemon_count += catch_pokemon(grid_of_houses, x, y)
         else:
             # ignore wrong input
             print("Invalid move")
        
     # DEBUG Visual representation
-    cols = len(world)
+    cols = len(grid_of_houses)
     rows = 0
     if cols:
-        rows = len(world[0])
+        rows = len(grid_of_houses[0])
     for j in range(rows):
         for i in range(cols):
-            print(world[i][j], end = "")
+            print(grid_of_houses[i][j], end = "")
         print()
     
-    print("Pokecount is:", pokeCount)
+    print("Pokemon count is:", pokemon_count)
     
 val = input("Enter Ash's movement sequence:")
-solve("NESSOONNNEEESSSSOOOONNNNNEEEEESSSSSSOOOOOONNNNNN")    
+solve(val)
+#solve("NESSOONNNEEESSSSOOOONNNNNEEEEESSSSSSOOOOOONNNNNN")    
